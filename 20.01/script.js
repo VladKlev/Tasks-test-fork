22a88ca64e14236f4e6198d936c68080f8485d07
VladKlev <vlad.klev08@gmail.com>
2023-01-21 10:33:28 +0100

    first changes
    some description changes

--- 20.01/script.js
+++ 20.01/script.js
@@ -1,13 +1,22 @@
-function updateTime() {
-    // Здесь должен быть элемент hours 
-    // Здесь должен быть элемент minutes 
-    // Здесь должен быть элемент seconds
+//   function updateHours() {
+//     console.log('updateHours', 123123)
+//     if (hours < 10) hours = '0' + hours;
+//     clock.children[0].innerHTML = hours;
+//   }
 
-    const clock = new Date();
+//   function updateMinutes() {
+//     if (minutes < 10) minutes = '0' + minutes;
+//     clock.children[1].innerHTML = minutes;
 
-    // getHours()
-    // getMinutes()
-    // getSeconds()
+//  } 
 
-}
-// Здесь должен быть задан интервал
+//   function updateSeconds() {
+//     if (seconds < 10) seconds = '0' + seconds;
+//     clock.children[2].innerHTML = seconds;
+
+//  }
+//   function updateMilliseconds() {
+//     if (milliseconds < 10) milliseconds = '0' + milliseconds;
+//     clock.children[3].innerHTML = milliseconds;
+//  } 
+  
